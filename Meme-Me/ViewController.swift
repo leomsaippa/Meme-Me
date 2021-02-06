048cfc1bbb7af218efdf1e4ffca4f7380d84b267
Leonardo Saippa <leonardomsaippa@gmail.com>
2021-02-06 16:13:26 -0300

    Initial Commit

---
+++ Meme-Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meme-Me
+//
+//  Created by Leonardo Saippa on 06/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
