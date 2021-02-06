2c9895f35ec476faf6111695864d83c3dd547125
Leonardo Saippa <leonardomsaippa@gmail.com>
2021-02-06 16:19:26 -0300

    Add layout and configure textFields

--- Meme-Me/ViewController.swift
+++ Meme-Me/ViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    @IBOutlet weak var pickImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureTextField(topText, text: "TOP")
+        configureTextField(bottomText, text: "BOTTOM")
+    }
+
+    func configureTextField(_ textField: UITextField, text: String) {
+        
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth: -5.0,
+        ]
+   
+        textField.defaultTextAttributes = memeTextAttributes
+
+        textField.text = text
+        textField.textAlignment = .center
+  
     }
 
 
