c3f994b10efdf015520fbed4ef6a852b47e16340
Leonardo Saippa <leonardomsaippa@gmail.com>
2021-02-06 16:39:16 -0300

    Fix keyboard bug

--- Meme-Me/ViewController.swift
+++ Meme-Me/ViewController.swift
@@ -8,18 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController,
-UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+UIImagePickerControllerDelegate & UINavigationControllerDelegate, UITextFieldDelegate {
 
 
     
     @IBOutlet weak var topText: UITextField!
     @IBOutlet weak var bottomText: UITextField!
     @IBOutlet weak var pickImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTextField(topText, text: "TOP")
         configureTextField(bottomText, text: "BOTTOM")
+        
+        bottomText.delegate = self
+        topText.delegate = self
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        subscribeToKeyboardNotifications()
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unsubscribeFromKeyboardNotifications()
+    }
+   
 
     func configureTextField(_ textField: UITextField, text: String) {
         
@@ -79,6 +93,47 @@ UIImagePickerControllerDelegate & UINavigationControllerDelegate {
         dismiss(animated: true, completion: nil)
     }
     
+    
+    func subscribeToKeyboardNotifications() {
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+
+        if bottomText.isEditing && view.frame.origin.y == 0 {
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+        
+    }
+
+   
+   @objc func keyboardWillHide(_ notification:Notification) {
+        if bottomText.isEditing && view.frame.origin.y != 0 {
+            view.frame.origin.y = 0
+        }
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    //When click return, hide keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
 
 }
 
